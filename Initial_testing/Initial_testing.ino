4f73f7f482ed698f8f72ddf186aa1adbd0173679
tobiasallen26 <ta502@cam.ac.uk>
2022-11-07 11:05:37 +0000

    Serial Communication test

--- Initial_testing/Initial_testing.ino
+++ Initial_testing/Initial_testing.ino
@@ -1,13 +1,18 @@
 void setup() {
-  // put your setup code here, to run once:
-
+	// put your setup code here, to run once:
+	Serial.begin(9600);
 }
 
 void line_sensor_reading() {
 	// module to read from a line sensor
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void serial_communication(){
+	// module to check communication across serial link
+	Serial.println('Hello World');
+}
 
+void loop() {
+	// put your main code here, to run repeatedly:
+	serial_communication();
 }
