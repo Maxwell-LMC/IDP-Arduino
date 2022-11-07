11c6152b40462cf8e9b08ece02334fc3b5ecce00
tobiasallen26 <ta502@cam.ac.uk>
2022-11-07 16:43:01 +0000

    latest Sensors control

--- Main programme/main/Sensors/Sensors.ino
+++ Main programme/main/Sensors/Sensors.ino
@@ -1,6 +1,6 @@
-int lineLpin = A0;
-int lineMpin = A1;
-int lineRpin = A2;
+int lineLpin = 2;
+int lineMpin = 3;
+int lineRpin = 4;
 
 int lineLvalue = 0;
 int lineMvalue = 0;
@@ -8,27 +8,25 @@ int lineRvalue = 0;
 
 int orientation = 0;
 
-int LINE_THRESHOLD = 500; // less than
-
 
 void setup() {
 	// put your setup code here, to run once:
-	Serial.begin(9600)
+	Serial.begin(9600);
 }
 
 void lineSensorsRead(){
 	// module to read from line sensors
-	lineLvalue = analogRead(lineLpin);
-	lineMvalue = analogRead(lineMpin);
-	lineRvalue = analogRead(lineRpin);
+	lineLvalue = digitalRead(lineLpin);
+	lineMvalue = digitalRead(lineMpin);
+	lineRvalue = digitalRead(lineRpin);
 }
 
 bool onLine(int value){
 	// module to say if the sensor is on the line
-	if (value < LINE_THRESHOLD){
-		return False;
+	if (value == 1){
+		return true;
 	};
-	return False
+	return false;
 }
 
 int relativeToLine(bool onLineL, bool onLineM, bool onLineR){
@@ -36,12 +34,12 @@ int relativeToLine(bool onLineL, bool onLineM, bool onLineR){
 	// returns 0 for straight, 1 for left and 2 for right
 	if (!onLineL && onLineM && !onLineR){
 		return 0;
-	} elseif (onLineL && onLineM && !onLineR){
-		return 1;
-	} elseif (!onLineL && onLineM && onLineR){
+	} else if (onLineL && onLineM && !onLineR){
 		return 2;
+	} else if (!onLineL && onLineM && onLineR){
+		return 1;
 	}
-	Serial.println("unknown orientation")
+	Serial.println("unknown orientation");
 	return 3;
 }
 
@@ -50,19 +48,19 @@ void responseToOrientation(int o){
 	switch (o) {
 		case 0:
 			// if the robot is straight
-			Serial.println('Straight');
+			Serial.println("Straight");
 			break;
 		case 1:
 			// if the robot is left
-			Serial.println('Left');
+			Serial.println("Left");
 			break;
 		case 2:
 			// if the robot is right
-			Serial.println('Right');
+			Serial.println("Right");
 			break;
-		case 3:
+		default:
 			// unkown orientation
-			Serial.println('Unknown');
+			Serial.println("Unknown");
 			break;
 	};
 }
@@ -71,6 +69,12 @@ void responseToOrientation(int o){
 void loop() {
 	// put your main code here, to run repeatedly:
 	lineSensorsRead();
+	Serial.print(lineLvalue);
+	Serial.print(" ");
+	Serial.print(lineMvalue);
+	Serial.print(" ");
+	Serial.println(lineRvalue);
+	delay(100);
 	orientation = relativeToLine(onLine(lineLvalue), onLine(lineMvalue), onLine(lineRvalue));
 	responseToOrientation(orientation);
 }
