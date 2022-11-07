434cb83dfbf9aaef7ed5a6d360c3d12d66845591
tobiasallen26 <ta502@cam.ac.uk>
2022-11-07 11:45:43 +0000

    1 line sensor

--- Initial_testing/Initial_testing.ino
+++ Initial_testing/Initial_testing.ino
@@ -1,10 +1,18 @@
+line1pin = A0;
+int line1Value = 0;
+
+
 void setup() {
 	// put your setup code here, to run once:
 	Serial.begin(9600);
+
+
 }
 
 void line_sensor_reading() {
 	// module to read from a line sensor
+	line1Value = analogRead(line1pin);
+	Serial.printLn(line1Value);
 }
 
 void serial_communication(){
@@ -13,10 +21,11 @@ void serial_communication(){
 }
 
 void LED_test(){
+	// module to test LEDs
 	digitalWrite(13, HIGH);       // sets the digital pin 13 on
-  delay(1000);                  // waits for a second
-  digitalWrite(13, LOW);        // sets the digital pin 13 off
-  delay(1000);                  // waits for a second
+	delay(1000);                  // waits for a second
+	digitalWrite(13, LOW);        // sets the digital pin 13 off
+	delay(1000);                  // waits for a second
 }
 
 void loop() {
