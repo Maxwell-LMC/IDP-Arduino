9a8e401c2d9a7a30d0ecba7fbcc4ce0c429c7e57
Maxwell <maxwell.limingchi@gmail.com>
2022-11-16 09:51:14 +0000

    added servo testing procedure

--- Initial_testing/Servo_testing/Servo_testing.ino
+++ Initial_testing/Servo_testing/Servo_testing.ino
@@ -5,30 +5,37 @@
 
 Servo servo;
 int feedbackPin = A0;
-float vol;
-float start_millis;
+float vol, sum, avg;
+float threshold_coarse = 50;
+float threshold_dense = 100;
 
 void setup() {
   Serial.begin(9600);
   servo.attach(10);
-  
 }
 
 void loop() {
-  servo.write(100);
-  vol = analogRead(feedbackPin);
-  Serial.println(vol);
-  delay(2000);
-  start_millis = millis();
-  servo.write(150);
-  while(servo.read() < 149){
-    Serial.println(servo.read());
+  servo.write(100); //relaxed position
+  delay(3000);
+  servo.write(150); //grabbed position
+  delay(1000);
+  for(int i = 1; i <= 20; i++){
+    vol = analogRead(feedbackPin);
+    sum += vol;
+    Serial.print("Average: ");
+    Serial.print(sum / i);
+    Serial.print(" Instant: ");
+    Serial.print(vol);
+    Serial.print("\n");
+    delay(250);
   }
-  Serial.print("time taken: ");
-  Serial.print(millis() - start_millis);
-  Serial.println(" ");
-  vol = analogRead(feedbackPin);
-  Serial.println(vol);
-  delay(2000);
-  
+  if((sum/20)> threshold_dense){
+      Serial.println("Result: Dense");
+  }
+  else if(sum > threshold_coarse){
+      Serial.println("Result: Coarse");
+  }
+  else{
+      Serial.println("No block detected");
+  }
 }
